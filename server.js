62b46abebf605636e447605a82dcc24c363750b0
salemking22 <brynjerel@gmail.com>
2025-07-08 08:01:07 +0200

    Add root route to show welcome message

--- server.js
+++ server.js
@@ -7,14 +7,21 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB!'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
 
-const contactsRouter = require('./routes/contacts');
+// Root route
+app.get('/', (req, res) => {
+    res.send('Contacts API is live 🎉');
+});
 
+// Routes
+const contactsRouter = require('./routes/contacts');
 app.use('/api/contacts', contactsRouter);
 
+// Start server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
